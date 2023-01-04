801a4e3a4ae8954c91195443147ebaecd3239f2d
Dunael <119928137+Dunael@users.noreply.github.com>
2023-01-04 13:58:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "AMI_ID" {
+    default = "AMI_ID"
+    description = "Identifiant de la VM nouvellement créée"
+}
